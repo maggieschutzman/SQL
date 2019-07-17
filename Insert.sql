dfff5bc03920e7f69f66ec08f77469fb7f71f674
Maggie Schutzman <maggieschutzman2@gmail.com>
2019-07-17 14:26:47 -0400

    inserts

---
+++ Insert.sql
@@ -0,0 +1,3 @@
+Insert into Employee (Lastname, Firstname, JobId) values ('Blem', 'Dog', 6);
+Insert into Employee (Lastname, Firstname, JobId) values ('Fork', 'Mike', 3);
+Insert into Employee (Lastname, Firstname, JobId) values ('Dasg', 'Fila', 12);
