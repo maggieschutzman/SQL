87faa50b23c8839b0316d0eca6479bb42bc5ebe9
Maggie Schutzman <maggieschutzman2@gmail.com>
2019-07-17 15:27:21 -0400

    update

---
+++ update.sql
@@ -0,0 +1,8 @@
+Select * from employee where id = 35
+
+Update Employee Set
+	Firstname = 'Wyle E.',
+	Lastname = 'Coyote'
+where Firstname = 'Mike' and Lastname = 'Fork'
+
+Select * from employee where id = 35
