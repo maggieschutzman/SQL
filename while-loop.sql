c8fb2dfc2fdc088a232566455ae157c4e292c0aa
Maggie Schutzman <maggieschutzman2@gmail.com>
2019-07-18 17:45:51 -0400

    while-loop

---
+++ while-loop.sql
@@ -0,0 +1,10 @@
+DECLARE @index int = 0;
+DECLARE @sum int = 0;
+
+
+WHILE @index <=15
+BEGIN
+	SET @sum = @sum + @index;
+	SET @index = @index + 1;
+END
+PRINT 'The sum of 1 to 15 is ' + cast(@sum as nvarchar(10));
