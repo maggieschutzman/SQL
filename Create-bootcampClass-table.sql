6e9bf0b0ef38b542e52f29c7956a6a3bac469347
Maggie Schutzman <maggieschutzman2@gmail.com>
2019-07-19 11:40:31 -0400

    Create-class-table

---
+++ Create-bootcampClass-table.sql
@@ -0,0 +1,11 @@
+INSERT into Students (Firstname, Lastname, Email, Phone)
+	Values ('Alex', 'Chan', 'frozenchanana@gmail.com', '5135605531'),
+	('Brailee', 'Begley', 'brail33na11@gmail.com', '5134351173'),
+	('Briana', 'Presley', 'presley.briana@gmail.com', '5132549808'),
+	('Glenn', 'Lindmark', 'lindmark421@gmail.com', '5134849323'),
+	('Jesse', 'Kyle', 'jkylecarpentry@gmail.com', '5136551990'),
+	('Maggie', 'Schutzman', 'maggieschutzman2@gmail.com', '8594797837'),
+	('Marianne', 'Lindardos', 'mlinardos.edu@gmail.com', '5137858178'),
+	('Titus', 'Moore', 'tmoore@edgewebware.com', '9374741083');
+
+	Select * From Students
